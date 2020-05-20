43ae209f33c5c5a78776a694a0680693f99cb0ee
LucasSimoesMartins <lucassimoes084@gmail.com>
2020-05-20 14:02:11 -0300

    Creating main layout

---
+++ app/src/main/java/com/lucassimoesmartins/flixguide/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.lucassimoesmartins.flixguide
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        configureSplash()
+    }
+
+    private fun configureSplash() {
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
